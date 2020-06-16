2cd04db4e7854929a9cae0eb01f8496c7f4b1636
Kevin Green <kgreen@HSSCO.local@it1-d.hssco.local>
2020-06-16 07:42:05 -0500

    Removed duplicate control flow lessons from root

--- boolean_operators_and.py
+++
@@ -1,9 +0,0 @@
-statement_one = False
-
-statement_two = True
-
-def graduation_reqs(credits,gpa):
-  if credits >= 120 and gpa >= 2.0:
-    return "You meet the requirements to graduate!"
-
-print(graduation_reqs(120,1.9))
--- boolean_operators_or.py
+++
@@ -1,5 +0,0 @@
-def graduation_mailer(gpa,credits):
-  if gpa >= 2.0 or credits >= 120:
-    return True
-
-print(graduation_mailer(1.9,120))
