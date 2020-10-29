584a15683326bfaf6815a6d5a4849e6f71701cff
Kevin Green <kgreen@HSSCO.local@it1-d.HSSCO.local>
2020-10-29 16:02:43 -0500

    fixed comment

--- lists/lists_challenge_1.py
+++ lists/lists_challenge_1.py
@@ -4,7 +4,7 @@
 
 # The function should add the last two elements of lst together and append the result to lst. It should do this process three times and then return lst.
 
-For example, if lst started as [1, 1, 2], the final result should be [1, 1, 2, 3, 5, 8].
+# For example, if lst started as [1, 1, 2], the final result should be [1, 1, 2, 3, 5, 8].
 
 def append_sum(lst):
   l = 0
